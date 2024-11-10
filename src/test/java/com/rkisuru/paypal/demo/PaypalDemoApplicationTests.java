427b62170309afff431c14be22f0f1b0419a6399
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-11-10 15:01:45 +0530

    init project

---
+++ src/test/java/com/rkisuru/paypal/demo/PaypalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkisuru.paypal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
